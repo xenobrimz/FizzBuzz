1f28cd88dd391bceccb11701357400a806681a06
XenoTheClapper <bobbyjackson13@live.com>
2021-05-24 08:56:10 -0500

    push

---
+++ FizzBuzz.js
@@ -0,0 +1,16 @@
+
+for(var i = 1; i <= 100;i++){
+
+    if(i%5 == 0 && i%3 == 0){
+        console.log("Fizz Buzz")
+    }
+    if(i%3 == 0){
+        console.log("Fizz")
+    }
+    else
+    if(i%5 == 0){
+        console.log("Buzz")
+    }
+    else
+    console.log(i);
+}
